e2ba55f9663dde8688a3e3b1779c0db2612d07fc
Wes Modes <wmodes@gmail.com>
2021-10-15 16:28:54 -0700

    testing vlc

---
+++ experiments/video/just-vlc-test.py
@@ -0,0 +1,23 @@
+
+
+# Start cvlc and keep it running
+#   cvlc blackrockstation/experiments/data/* --random --loop --fullscreen --no-video-title --autoscale --intf http --http-host localhost --http-port 9090 --http-password vlc
+# Use the http interface to send comnmands
+#   https://wiki.videolan.org/Interfaces/
+#   https://wiki.videolan.org/VLC_HTTP_requests/
+#   ex: curl http://localhost:9090/requests/playlist.xml -u :vlc
+
+# add <mrl> to playlist and start playback: status.xml?command=in_play&input=<mrl>
+
+# add <mrl> to playlist:    status.xml?command=in_enqueue&input=<mrl>
+
+# empty playlist:  status.xml?command=pl_empty
+
+# toggle random playback:   status.xml?command=pl_random
+
+# toggle loop:  status.xml?command=pl_loop
+
+# toggle fullscreen:    status.xml?command=fullscreen
+
+# get <dir>'s filelist:     browse.xml?dir=<dir>
+# curl -u :vlc http://localhost:9090/requests/browse.json?dir=blackrockstation/experiments/data
