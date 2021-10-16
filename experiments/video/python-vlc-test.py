6ed16055876ef07e6807ccad2943c68642eab95a
Wes Modes <wmodes@gmail.com>
2021-10-15 18:52:27 -0700

    Update python-vlc-test.py

--- experiments/video/python-vlc-test.py
+++ experiments/video/python-vlc-test.py
@@ -1,9 +1,34 @@
 
-# importing vlc module
+# importing time and vlc
+import time
 import vlc
 
-# creating vlc media player object
-media = vlc.MediaPlayer("../data/070_SHARP_Titles_Flower_Titles.mp4")
+# method to play video
+def video(source):
 
-# start playing video
-media.play()
+    # creating a vlc instance
+    vlc_instance = vlc.Instance()
+
+    # creating a media player
+    player = vlc_instance.media_player_new()
+
+    # creating a media
+    media = vlc_instance.media_new(source)
+
+    # setting media to the player
+    player.set_media(media)
+
+    # play the video
+    player.play()
+
+    # wait time
+    time.sleep(0.5)
+
+    # getting the duration of the video
+    duration = player.get_length()
+
+    # printing the duration of the video
+    print("Duration : " + str(duration))
+
+# call the video method
+video("../data/070_SHARP_Titles_Flower_Titles.mp4")
