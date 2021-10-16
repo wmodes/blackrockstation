53b2b2c74196e7d55ca0b3300c075febe98badda
Wes Modes <wmodes@gmail.com>
2021-10-15 18:01:35 -0700

    testing

--- experiments/video/just-vlc-test.js
+++ experiments/video/just-vlc-test.js
@@ -1,19 +1,31 @@
 $(document).ready(function()
 {
-  url = "http://localhost:9090/status.json"
+  url = "http://localhost:9090/"
+  var username = $('input[name=username]').val();
+  var password = $('input[name=password]').val();
+  //
+  var endpoint = $('input[name=endpoint]').val();
+  var url = url + endpoint;
+  //
+  var data = {}
+  var command = $('input[name=command]').val();
+  if (command) {
+    data.command = command;
+  }
+  var input = $('input[name=input]').val();
+  if (input) {
+    data.input = input;
+  }
 
   $('#submit').click(function() {
     console.log("click");
     $.ajax(
       {
-        'password' : $('input[name=password]').val(),
-        'username' : $('input[name=username]').val(),
+        'username' : username,
+        'password' : password,
         'url'      : url,
         'type'     : 'GET',
-        'data'     : {
-          'command' : $('input[name=command]').val(),
-          'input'   : $('input[name=input]').val()
-        },
+        'data'     : data,
         'success'  : function(results){
           $("#output").html(JSON.stringify(results));
         },
@@ -22,7 +34,5 @@ $(document).ready(function()
         }
       }
     );
-
-    return false;
   });
 });
