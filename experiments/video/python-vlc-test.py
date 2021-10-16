80a456333ab0ee52ab471b51afd6329f67158166
Wes Modes <wmodes@gmail.com>
2021-10-15 18:34:06 -0700

    playing w video

---
+++ experiments/video/python-vlc-test.py
@@ -0,0 +1,9 @@
+
+# importing vlc module
+import vlc
+
+# creating vlc media player object
+media = vlc.MediaPlayer("../data/070_SHARP_Titles_Flower_Titles.mp4")
+
+# start playing video
+media.play()
