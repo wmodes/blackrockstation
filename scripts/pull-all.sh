8cc2e682080c650bc23e2a04932479aa28b35ffa
Wes Modes <wmodes@gmail.com>
2021-11-02 18:02:03 -0700

    JS static files

---
+++ scripts/pull-all.sh
@@ -0,0 +1,9 @@
+ssh pi@brs-scheduler.local 'cd blackrockstation; git pull'
+ssh pi@brs-announce.local 'cd blackrockstation; git pull'
+ssh pi@brs-bridge.local 'cd blackrockstation; git pull'
+ssh pi@brs-crossing.local 'cd blackrockstation; git pull'
+ssh pi@brs-lights.local 'cd blackrockstation; git pull'
+ssh pi@brs-radio.local 'cd blackrockstation; git pull'
+ssh pi@brs-train.local 'cd blackrockstation; git pull'
+ssh pi@brs-tv.local 'cd blackrockstation; git pull'
+
