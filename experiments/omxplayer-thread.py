85e324f95d1a0ce04e1e0ddd329fd6da391efa8c
Wes Modes <wmodes@gmail.com>
2021-12-02 17:22:57 -0800

    Update omxplayer-thread.py

--- experiments/omxplayer-thread.py
+++ experiments/omxplayer-thread.py
@@ -1,13 +1,25 @@
 
 import subprocess
+import os
+import signal
 import threading
 from time import sleep
 
+player = "/home/pi/blackrockstation/omxplaylist/omxplaylist.py"
+opt = ["--loop", "--random"]
+playlist = "/home/pi/blackrockstation/experiments/audio/playlist/"
+cmd = [player] + opt + [playlist]
+
+process = None
+
 def runomx():
-    subprocess.Popen(["/usr/bin/omxplayer", "wlac-grizzard-60-u.mp3"])
+    #subprocess.Popen([player, playlist])
+    process = subprocess.Popen(cmd, preexec_fn=os.setsid)
 
 def killomx():
-    subprocess.Popen(["killall", "omxplayer.bin"])
+    # subprocess.Popen(["killall", "omxplayer.bin"])
+    # Send the signal to all the process groups
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
 thread_obj = threading.Thread(target=runomx)
 thread_obj.start()
