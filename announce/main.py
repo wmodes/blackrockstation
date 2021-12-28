219b3da40bdd98dd6ec989cc70ef8a6e8c63f8e7
Wes Modes <wmodes@gmail.com>
2021-12-27 20:07:18 -0800

    Update main.py

--- announce/main.py
+++ announce/main.py
@@ -64,10 +64,10 @@ for host in config.CONTROLLERS.values():
     domain_list.append("http://" + host["server"] + ':' + str(host["port"]))
     domain_list.append("http://" + host["altserv"] + ':' + str(host["port"]))
 print(domain_list)
-CORS(app, supports_credentials=True, origins=domain_list,
-    allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"])
+CORS(app, supports_credentials=True, origins=domain_list)
 # CORS(app, origins="http://brs-scheduler.local:8080/")
 # app.config['CORS_HEADERS'] = 'Content-Type'
+app.config('CORS_ALLOW_HEADERS') = ["Content-Type", "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"]
 
 @app.route("/cmd",methods = ['POST', 'GET'])
 def cmd():
